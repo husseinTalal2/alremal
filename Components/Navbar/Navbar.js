697fa006aae424169e1fadd53d0515deeccff4c0
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2021-02-02 16:19:41 +0300

    fixing responsive nav

--- Components/Navbar/Navbar.js
+++ Components/Navbar/Navbar.js
@@ -10,8 +10,8 @@ function Navbar() {
     };
     return (
         <>
-            <header className="flex flex-col justify-center items-center">
-                <div className="md:w-4/5 h-24 flex justify-between items-center">
+            <header className="flex md:flex-col justify-between md:justify-center items-center text-white bg-black bg-opacity-30">
+                <div className="md:w-4/5 h-24 flex justify-between items-center md:ml-0 ml-5">
                     <div className="logo">
                         <Link href="/">
                             <a>
@@ -23,13 +23,13 @@ function Navbar() {
                             </a>
                         </Link>
                     </div>
-                    <div className="flex ">
-                        <Phone /> <span>+964 (770) 000-0000</span>
+                    <div className="hidden md:flex">
+                        <Phone /> <span className="ml-5">+964 (770) 000-0000</span>
                     </div>
                 </div>
-                <nav className="md:w-4/5 h-20 flex justify-between items-center bg-white">
+                <nav className="md:w-4/5 h-20 flex justify-between items-center">
                     <div className="w-full flex items-center justify-between">
-                        <div className="w-full nav-items hidden md:flex justify-between">
+                        <div className="w-full nav-items hidden md:flex justify-between ">
                             <ActiveLink activeClassName="active" href="/">
                                 <a className="nav-item mx-2 hover:text-gray-700">
                                     HOME
@@ -67,19 +67,21 @@ function Navbar() {
                             </ActiveLink>
                         </div>
                         <div
-                            className="nav-toggle md:hidden cursor-pointer"
+                            className="nav-toggle md:hidden cursor-pointer mr-5"
                             onClick={() => toggleNav()}
                         >
                             <Menu />
                         </div>
                     </div>
                 </nav>
-                <div
+               
+            </header>
+            <div
                     className={
-                        !toggle && "mobile-nav bg-white md:hidden hidden"
+                        !toggle && "mobile-nav bg-black md:hidden hidden"
                     }
                 >
-                    <div className="flex flex-col mobile-nav bg-white h-100 w-full items-center pb-10">
+                    <div className="flex flex-col mobile-nav bg-black bg-opacity-25 h-100 w-full items-center pb-10 text-white">
                         <ActiveLink activeClassName="active" href="/">
                             <a className="nav-item mx-2 hover:text-gray-700 pt-5 text-center w-max align-center">
                                 HOME
@@ -111,7 +113,6 @@ function Navbar() {
                         </ActiveLink>
                     </div>
                 </div>
-            </header>
         </>
     );
 }
