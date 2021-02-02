506cdb27e1e81778d540f3a3bb3ab134f865cd5b
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2021-02-02 16:19:59 +0300

    styling the home

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,14 @@ import Navbar from "../Components/Navbar/Navbar";
 export default function Home() {
     return (
         <>
-            <Navbar />
+            <div className="header h-screen">
+              <div className="overlay h-full flex flex-col">
+                <Navbar />
+                <div className="h-full w-100 flex justify-center items-center">
+                  <p className="text-white text-3xl font-bold uppercase">something here</p>
+                </div>
+              </div>
+            </div>
             <main></main>
         </>
     );
